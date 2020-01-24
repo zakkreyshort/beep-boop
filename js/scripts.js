f0b34f11c94e5564b77463fc8fa6e59ea96982ee
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 11:13:49 -0800

    start of a good base for scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
+function boopmybeep(num) {
+  var userNumber = parseInt($("#onlyInput").val());
+  var numbArr = []
+
+  for (var number = 0; number <= userNumber; number ++)
+
+  numbArr.push(number);
+  console.log(numbArr);
+}
+
+
+
 $(document).ready(function(){
   $("#inputForm").submit(function(event){
     event.preventDefault();
-    var inputP = $("#onlyInput").val();
-    console.log(inputP)
+    var inputP = parseInt($("#onlyInput").val());
+    
+    
+    $("#output").append(inputP);
   })
 })
