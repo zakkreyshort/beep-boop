af70c13ac6a333017d0b79a383185e0f9dff0a99
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 15:03:18 -0800

    finsh back logic, creat empty array to push into, create for statemet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-// Front end
+// back
+function please(str){
+  var emptyArr = [];
+  for(var index = 0; index <= str; index ++){
+    emptyArr.push(index);
+  }
+  // logic needs to go in decending order to meet specs
+  for (var i = 0; i <= str; i ++) {
+    var makeString = i.toString();
+    if (makeString.includes("3")) {
+      emptyArr[i] = "im sorry dave i cannot do that.";
+    } else if (makeString.includes("2")) {
+      emptyArr[i] = "Boop!";
+    } else if (makeString.includes("1")) {
+      emptyArr[i] = "Beep!";
+    }
+  }
+  return emptyArr;
+}
+
+// Front
 $(document).ready(function(){
   $(".inputHere").submit(function(event){
     event.preventDefault();
     var enterNumber = parseInt($("input#numberHere").val());
+    $("#finalResult").text(please(enterNumber));
   })
-  $("$finalResult").text(FUNCTIONNAMEHERE(enterNumber));
 })
