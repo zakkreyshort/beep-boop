514039f97723c0d2a68b32055e93e0e578009871
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 14:31:03 -0800

    start over, recreate html page, start scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,8 @@
-// create behvaior for 1,2,3
-var enterNum = parseInt($("#onlyOutput").val());
-  if(enterNum === 1){
-    return "Beep!"
-  } else if (enterNum === 2){
-    return "Boop!"
-  } else if (enterNum === 3){
-    return "I'm sorry, Dave. I'm afraid I can't do that."
-  }
-}
-
-
-// front end 
+// Front end
 $(document).ready(function(){
-  $("#inputForm").submit(function(event){
+  $(".inputHere").submit(function(event){
     event.preventDefault();
-    var inputP = parseInt($("#onlyOutput").val());
-    $("#output").text(enterNum);
+    var enterNumber = parseInt($("input#numberHere").val());
   })
+  $("$finalResult").text(FUNCTIONNAMEHERE(enterNumber));
 })
