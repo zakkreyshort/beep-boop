a480dd59959e69b44fa9643fb93ea241a549d2f2
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 13:52:29 -0800

    lost.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
+// create behvaior for 1,2,3
+var enterNum = parseInt($("#onlyOutput").val());
+  if(enterNum === 1){
+    return "Beep!"
+  } else if (enterNum === 2){
+    return "Boop!"
+  } else if (enterNum === 3){
+    return "I'm sorry, Dave. I'm afraid I can't do that."
+  }
+}
 
 
+// front end 
 $(document).ready(function(){
-  $("#inputForm").submit(function(){
-    
-    var inputP = parseInt($("#onlyInput").val());
+  $("#inputForm").submit(function(event){
     event.preventDefault();
-    $("#output").text(inputP);
+    var inputP = parseInt($("#onlyOutput").val());
+    $("#output").text(enterNum);
   })
 })
