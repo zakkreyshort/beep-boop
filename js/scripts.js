3e6ae44518a7fe77f5741366a10ee89df4f2459c
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 10:51:18 -0800

    start over JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,7 @@
 $(document).ready(function(){
-  $("form#inputForm").submit(function(event){
-  event.preventDefault();
-  var compare = $("#onlyInput").val();
-  var numArr = onlyInput.split(compare);
-
-  for(var i = 0; i < numArr; i++){
-    var beep = [1];
-    if(numb){
-      for(var i = 0; i < numArr; i++){
-        if(numb === beep){
-          return "Beep";
-        }
-      }
-    }
-  }
-
-  $("#output").text(result);
+  $("#inputForm").submit(function(event){
+    event.preventDefault();
+    var inputP = $("#onlyInput").val();
+    console.log(inputP)
   })
 })
-
-// for (var i=0; i < #onlyInput; i++)
-// {}
