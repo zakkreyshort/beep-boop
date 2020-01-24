9180ecf69d05730d46f5aaec4820f6171716686e
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 13:10:30 -0800

    scrap back-end. start over.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,4 @@
-// back end
 
-
-
-// function for beep/boop/dave
-function listNumb(input){
-  var numar = [];
-  var input = parseInt($("#onlyInput").val());
-  for(var i = "0"; i < input.length; i++){
-    if (input === "1"){
-      $("#output").text("Beep!")
-    } else if (input === "2") {
-      $("#output").text("Boop!")
-    } else if (input === "3") {
-      $("#output").text("I'm sorry, Dave. I'm afraid I can't do that.")
-    } else{
-      $("#output").text(input);
-    };
-  };
-}
-
-// frontend
 
 $(document).ready(function(){
   $("#inputForm").submit(function(){
