6017e9716ce4eed4eb2907996758e570cf41732c
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 11:23:37 -0800

    FINALLY MADE A BREAKTHROUGH IN SCRIPTS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function boopmybeep(num) {
+function boopmybeep(number) {
   var userNumber = parseInt($("#onlyInput").val());
   var numbArr = []
 
@@ -8,14 +8,21 @@ function boopmybeep(num) {
   console.log(numbArr);
 }
 
+var exceptions = function(userNumber){
+  if (userNumber ===1){
+    $("#output").append("Beep!");
+  }
+}
 
+// frontend
 
 $(document).ready(function(){
   $("#inputForm").submit(function(event){
     event.preventDefault();
     var inputP = parseInt($("#onlyInput").val());
+    var result = exceptions(inputP);
     
     
-    $("#output").append(inputP);
+    $("#output").append(result);
   })
 })
