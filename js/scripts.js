374d4d7559d79a82cd7dfe952c6119640418100f
Zakkrey Short <zakkreyjshort@gmail.com>
2020-01-24 10:16:13 -0800

    try new JS methods, try new methods.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
-// bus log
-var outputNum = function(compare){
-  if (compare === 1){
-    return beep;
-  } else if (compare === 2){
-    return boop;
-  } else {
-  }
-}
-
-
-// user interface
 $(document).ready(function(){
   $("form#inputForm").submit(function(event){
   event.preventDefault();
-  var compare = $("input#onlyInput").val();
-  var result = outputNum(compare);
+  var compare = $("#onlyInput").val();
+  var numArr = onlyInput.split(compare);
+
+  for(var i = 0; i < numArr; i++){
+    var beep = [1];
+    if(numb){
+      for(var i = 0; i < numArr; i++){
+        if(numb === beep){
+          return "Beep";
+        }
+      }
+    }
+  }
 
   $("#output").text(result);
   })
